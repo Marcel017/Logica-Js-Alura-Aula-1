9183accca2d39c098e14266395380f47805e53db
Marcel017 <mcelogabriel17@gmail.com>
2025-09-29 21:47:04 -0300

    Aula-2-check
    Aprendi que função é algo muito importante, pois ela serve como uma verificação, é nomeclatura tbm

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ titulo.innerHTML = 'Jogo do Numero Secreto';
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um numero entre 1 e 10'; 
 
+function verificarChute() {
+    console.log('o botão foi clicado')
+}
+
