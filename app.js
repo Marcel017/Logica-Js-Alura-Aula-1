df9e5188421b2901e1fa6524a036dc49b0ef4e1d
Marcel017 <mcelogabriel17@gmail.com>
2025-09-29 21:58:24 -0300

    Aula-3-check
    Da pra evitar codigos repetitivos com funções, aprendi a comentar tbm

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-let titulo = document .querySelector('h1');
-titulo.innerHTML = 'Jogo do Numero Secreto';
+//let titulo = document .querySelector('h1');
+//titulo.innerHTML = 'Jogo do Numero Secreto';
 
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um numero entre 1 e 10'; 
+
+function exibirTextoNaTela(tag) {
+    let campo= document.querySelector(tag); 
+    campo.innerHTML = texto; 
+}
+
+exibirTextoNaTela('h1')
+exibirTextoNaTela('p')
 
 function verificarChute() {
     console.log('o botão foi clicado')
