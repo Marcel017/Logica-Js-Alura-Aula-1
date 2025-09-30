cf07417b3b59c5a4590e366070ee95d294dc4796
Marcel017 <mcelogabriel17@gmail.com>
2025-09-30 06:32:29 -0300

    Aula-5-check
    Aprendendo mais sobre if e else, meu codigo agora esta ficando estruturando, ainda não criamos o alert, mas  o jogo esta "jogavel"

--- app.js
+++ app.js
@@ -10,7 +10,17 @@ exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto);
+    
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou'); 
+        exibirTextoNaTela('p', 'Voçê descobriu o numero secreto');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'o número secreto e menor')
+        } else {
+            exibirTextoNaTela('p', 'O numero secreto e maior')
+        }
+    }
 }
 
 function gerarNumeroAleatorio() {
