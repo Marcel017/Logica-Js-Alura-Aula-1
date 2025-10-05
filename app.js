1217ba0bb703ee55b94c7956068c6a043a7839ab
Marcel017 <mcelogabriel17@gmail.com>
2025-10-04 23:57:56 -0300

    Aula-12=Checlk
    Coloquei uma Voz Feminina

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let tentativas = 1;
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    reponsiveVoice.speak(texto)
+    reponsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2})
 }
 
 function exibirMensageinicial() {
